22156976ec9e5dc0acf33a611d7275887e8a6e44
nigh <jiyucheng007@gmail.com>
2018-10-15 18:58:32 +0800

    update
    Signed-off-by: nigh <jiyucheng007@gmail.com>

--- main.lua
+++ main.lua
@@ -4,7 +4,19 @@ local bump = require 'bump'
 local world = bump.newWorld(25)	-- 64 ~ 70 工作异常
 local ground = {
 	name="ground_tile",
-	x=-100,y=700,w=2000,h=2
+	x=-100,y=700,w=2000,h=5
+}
+local box1 = {
+	name="box1",
+	w=10,h=32
+}
+local box2 = {
+	name="box2",
+	w=5,h=64
+}
+local box3 = {
+	name="box3",
+	w=200,h=129
 }
 function love.load()
 	print("test")
@@ -12,22 +24,43 @@ function love.load()
 	love.math.setRandomSeed( 6173420+love.timer.getTime( ) )
 	player:init(world)
 	world:add(ground,ground.x,ground.y,ground.w,ground.h)
+	waog=function(t,x)world:add(t,x,ground.y-t.h,t.w,t.h)end
+	waog(box1,400)
+	waog(box2,600)
+	waog(box3,900)
 end
 
 function love.update( dt )
-	
+	if love.keyboard.isDown('a') then
+		player:run("left")
+	elseif love.keyboard.isDown('d') then
+		player:run("right")
+	else
+		player:run()
+	end
 	player:update(dt)
 end
 
 function love.draw( ... )
-
 	love.graphics.setColor(1, 1, 1)
-	love.graphics.rectangle("fill",ground.x,ground.y,ground.w,ground.h)
+	local tab,len = world:getItems()
+	for i=1,len do
+		if tab[i].name~="player" then
+			love.graphics.rectangle("fill",world:getRect(tab[i]))
+		end
+	end
+	-- love.graphics.rectangle("fill",ground.x,ground.y,ground.w,ground.h)
 	player:draw()
 end
 
 function love.keypressed( key, scancode, isrepeat )
 	if key == 'w' and not isrepeat then
-		player:jump()
+		player:jumpstart()
+	end
+end
+
+function love.keyreleased( key, scancode )
+	if key == 'w' then
+		player:jumpstop()
 	end
 end
--- player.lua
+++ player.lua
@@ -2,45 +2,137 @@
 local player={}
 
 function player:init(world)
+	self.name = "player"
 	self.x = 50
 	self.y = 50
 	self.w = 20
 	self.h = 36
-	self.vx = 50
+	self.vx = 0
 	self.vy = 0
+	self.jumpcount = 0
+	self.jpt = 0	-- 大小跳时间阈值
+	self.state = "init"
+	self.jumpstate = ""
 	self.world = world
 	self.world:add(self,self.x,self.y,self.w,self.h)
+
+	self:effectInit()
 end
 
-function player:jump()
-	self.vy = -400
+function player:effectInit()
+	self.jumpeffect = love.graphics.newParticleSystem(love.graphics.newImage( "dot.png" ),40)
+	self.jumpeffect:setOffset( 12, 4 )
+	self.jumpeffect:setParticleLifetime(.2,.3)
+	self.jumpeffect:setEmissionRate(400)
+	self.jumpeffect:setEmitterLifetime(.2)
+	self.jumpeffect:setColors(
+		1, .7, .5, 1, 
+		1, .4, .5, 0.2,
+		1, 1, 1, 0
+	)
+	self.jumpeffect:setSizes(1,2,4)
+	self.jumpeffect:setDirection( math.pi )
+	self.jumpeffect:setSpread( math.pi*0.3 )
+	self.jumpeffect:setSpeed( -200,200 )
+	self.jumpeffect:setLinearAcceleration(0,666)
+	self.jumpeffect:setEmissionArea( "uniform", 20, 5, 0 )
+	self.jumpeffect:setLinearDamping(3)
+	self.jumpeffect:stop()
 end
 
+function player:jumpeffectAdd(x,y)
+	self.jumpeffect:reset()
+	self.jumpeffect:setPosition(x,y)
+	self.jumpeffect:start()
+end
+
+function player:jumpstart()
+	if self.jumpcount<1 then
+		self.vy = -300
+		if self.state=="air" then
+			self:jumpeffectAdd(self.x+self.w*0.5,self.y+self.h)
+			self.jumpcount = self.jumpcount+1
+		end
+		self.jumpstate="jumpstart"
+		self.jpt = 0.14
+	end
+end
+
+function player:jumpstop()
+	if self.jumpstate=="jumpstart" then
+		self.jumpstate = ""
+	end
+end
+
+function player:jumphigh()
+	-- self.vy = -300
+	self.jumpstate="jumphigh"
+end
+
+function player:run(dir)
+	if dir=="left" then
+		self.vx = -300
+	elseif dir=="right" then
+		self.vx = 300
+	else
+		self.vx = 0
+	end
+end
 
 function player:draw()
 	love.graphics.rectangle("line",self.x,self.y,self.w,self.h)
+	local mode = love.graphics.getBlendMode()
+	love.graphics.setBlendMode( "add" )
+	love.graphics.draw(self.jumpeffect,0,0)
+	love.graphics.setBlendMode( mode )
 end
 
 
+height={0,0,0}
 function player:update(dt)
-	self.onGround = self.isTouchGround
-	if self.onGround ~= true then
-		self.vy = self.vy + 1000*dt
+	self.jumpeffect:moveTo(self.x+self.w*0.5,self.y+self.h)
+	self.jumpeffect:update(dt)
+	if self.jumpstate=="jumpstart" then
+		self.jpt = self.jpt-dt
+		if self.jpt<=0 then
+			self:jumphigh()
+		end
 	end
+	-- 掉落测试
+	local _, actualY = self.world:check(self, self.x, self.y+10)
+	self.onGround = self.y==actualY
+
+	local g = 0
+	if self.onGround == true then
+		if self.jumpstate~="jumpstart" then
+			self.vy = 0
+		end
+		self.jumpcount = 0
+		self.state = "ground"
+	else
+		self.state = "air"
+	end
+	if self.jumpstate~="jumpstart" then
+		g = 1500
+	end
+	-- print("dt:"..dt.."\tvy:"..self.vy.."\tdh:"..self.vy*dt + 0.5*a*dt*dt)
+	player:move(self.vx * dt,self.vy*dt + 0.5*g*dt*dt)
+	self.vy = self.vy+g*dt
 	if self.vy>750 then self.vy=750 end
-	self.isTouchGround = false
-	player:move(self.vx * dt,self.vy * dt)
-	print(self.vy)
+	height[1] = height[2]
+	height[2] = height[3]
+	height[3] = self.y
+	if height[2]<height[1] and height[2]<height[3] then
+		print("jump:" .. 664-height[2])
+	end
 end
 
 local function col_solve(item, other)
 	if other.name == "ground_tile" then
 		item.vy = 0
-		item.isTouchGround = true
 		if item.onGround == true then
 			return "slide"
 		else
-			item.onGround = true
 			return "touch"
 		end
 	else
@@ -52,9 +144,9 @@ function player:move(dx,dy)
 	local actualX, actualY, cols, len = self.world:move(self, self.x+dx,self.y+dy,col_solve)
 	self.x, self.y = actualX, actualY
 	-- self.world:update(self,self.x, self.y)
-	for i=1,len do
-		print('collided with ' .. tostring(cols[i].other))
-	end
+	-- for i=1,len do
+	-- 	print('collided with ' .. tostring(cols[i].other))
+	-- end
 end
 
 return player
