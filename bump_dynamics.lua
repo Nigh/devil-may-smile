e789c53772835c0c3caff7209d7beb13c40da9b6
Nigh <jiyucheng007@gmail.com>
2018-10-23 01:28:19 +0800

    add bd lib

---
+++ bump_dynamics.lua
@@ -0,0 +1,11 @@
+
+local bd = {
+    _VERSION     = 'bump_dynamics alpha0',
+    _URL         = '',
+    _DESCRIPTION = 'A simple dynamics library base on bump',
+    }
+
+local bump = require 'bump'
+
+
+return bd
